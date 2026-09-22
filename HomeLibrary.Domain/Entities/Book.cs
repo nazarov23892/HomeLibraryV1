df199584c74c641e3eed9d8477f07eaf0ac20e8d
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-22 12:08:37 +0500

    + Доменная модель книги.

---
+++ HomeLibrary.Domain/Entities/Book.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeLibrary.Domain.Entities;
+
+public class Book
+{
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Название.
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Автор.
+    /// </summary>
+    public string Author { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Год издания.
+    /// </summary>
+    public int PublishYear { get; set; }
+
+    /// <summary>
+    /// Оглавление.
+    /// </summary>
+    /// <remarks>Формат XML.</remarks>
+    public string TableOfContents { get; set; } = string.Empty;
+}
