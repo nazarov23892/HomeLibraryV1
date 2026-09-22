0884b266bb10249d04ced6d9b8010294f52234dd
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-22 15:28:16 +0500

    + Заполнение БД тестовыми данными.

--- HomeLibrary.Web/Program.cs
+++ HomeLibrary.Web/Program.cs
@@ -3,6 +3,7 @@ using HomeLibrary.AL.Services;
 using HomeLibrary.AL.Services.Concrete;
 using HomeLibrary.DAL.Concrete;
 using HomeLibrary.DAL.DbContexts;
+using HomeLibrary.Web.SeedData;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,5 +26,13 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 app.MapRazorPages();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await SeedData.RunSeed(dbContext, authorsCount: 5, booksCount: 10, logger);
+}
 
 app.Run();
---
+++ HomeLibrary.Web/SeedData/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using HomeLibrary.DAL.DbContexts;
+using HomeLibrary.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeLibrary.Web.SeedData;
+
+public static class SeedData
+{
+    public static async Task RunSeed(
+        ApplicationDbContext dbContext, 
+        int authorsCount,
+        int booksCount,
+        ILogger logger, 
+        CancellationToken cancellationToken = default)
+    {
+        var exists = await dbContext.Authors.AnyAsync(cancellationToken);
+        if(exists )
+        {
+            logger.LogInformation("Seeding was skipped.");
+            return;
+        }
+
+        await SeedAuthors(dbContext, authorsCount, logger, cancellationToken);
+        await SeedBooks(dbContext, booksCount, logger, cancellationToken);
+    }
+
+    static async Task SeedAuthors(
+        ApplicationDbContext dbContext, int count, ILogger logger, CancellationToken cancellationToken)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            dbContext.Authors.Add(
+                new Author()
+                {
+                    Name = $"author-{1 + i}",
+                });
+        }
+        var totalCount = await dbContext.SaveChangesAsync(cancellationToken);
+        logger.LogInformation("Seeding authors done. Records: {Count}", totalCount);
+    }
+
+    static async Task SeedBooks(
+        ApplicationDbContext dbContext, int count, ILogger logger, CancellationToken cancellationToken)
+    {
+        var authors = await dbContext.Authors
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        foreach (var author in authors) 
+        {
+            for (var i = 0; i < count; i++)
+            {
+                dbContext.Books.Add(
+                    new Book()
+                    {
+                        Title = $"book-{1 + i}",
+                        AuthorId = author.Id,
+                        PublishYear = 2000 + i,
+                        TableOfContents = "<root/>",
+                    });
+            }
+        }
+        var totalCount = await dbContext.SaveChangesAsync(cancellationToken);
+        logger.LogInformation("Seeding books done. Records: {Count}", totalCount);
+    }
+}
