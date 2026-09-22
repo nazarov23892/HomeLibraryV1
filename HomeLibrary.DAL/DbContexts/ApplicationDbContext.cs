cf1601d64eb3cf8440ce2095dfb5e138ad5f7223
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-22 12:29:42 +0500

    + EF Context.

---
+++ HomeLibrary.DAL/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using HomeLibrary.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeLibrary.DAL.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Book> Books => Set<Book>();
+
+    /// <inheritdoc/>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(
+          entity =>
+          {
+              entity.Property(p => p.TableOfContents)
+                  .HasColumnType("xml");
+
+              entity.HasOne(p => p.Author)
+                  .WithMany()
+                  .HasForeignKey(p => p.AuthorId)
+                  .OnDelete(DeleteBehavior.Restrict);
+          });
+    }
+}
