2b2a22d063b21868633dfa67e55e50432611f395
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-22 14:36:28 +0500

    + Страница списка книг.

---
+++ HomeLibrary.AL/DTOs/BookListDto.cs
@@ -0,0 +1,29 @@
+﻿namespace HomeLibrary.AL.DTOs;
+
+/// <summary>
+/// Списочная модель книги.
+/// </summary>
+public class BookListDto
+{
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Название.
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Идентификатор автора.
+    /// </summary>
+    public long AuthorId { get; set; }
+
+    /// <summary>
+    /// Автор.
+    /// </summary>
+    public string Author { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Год издания.
+    /// </summary>
+    public int PublishYear { get; set; }
+}
--- HomeLibrary.Web/Program.cs
+++ HomeLibrary.Web/Program.cs
@@ -11,8 +11,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(
         opts.UseSqlServer(connectionString);
     });
 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapRazorPages();
 
 app.Run();
