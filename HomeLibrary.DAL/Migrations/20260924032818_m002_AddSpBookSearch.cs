a920f51b1f00863da6108d39f1f67e07a36deca0
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-24 16:43:34 +0500

    + Поиск по оглавлению.

--- HomeLibrary.DAL/Migrations/20260924032818_m002_AddSpBookSearch.cs
+++ HomeLibrary.DAL/Migrations/20260924032818_m002_AddSpBookSearch.cs
@@ -24,20 +24,23 @@ CREATE PROCEDURE [dbo].[Books_Search]
 	@TotalCount INT OUTPUT
 AS
 BEGIN
+  DECLARE @MinLenghForSearch int = 3;
   SELECT @TotalCount = COUNT(*) 
   FROM Books b inner join Authors a on a.[Id] = b.[AuthorId]
-  WHERE @SearchString IS NULL 
+  WHERE @SearchString IS NULL OR LEN(@SearchString) < @MinLenghForSearch
   OR LOWER(b.[Title]) LIKE CONCAT('%', LOWER(@SearchString), '%')
   OR LOWER(a.[Name]) LIKE CONCAT('%', LOWER(@SearchString), '%')
+  OR b.TableOfContents.exist('//*[contains(text()[1], sql:variable(""@SearchString""))]') = 1;
 
   SELECT
-    b.[Id], b.[Title], b.[PublishYear], b.[AuthorId], a.[Id], a.[Name], COUNT(*) OVER()
+    b.[Id], b.[Title], b.[PublishYear], b.[AuthorId], a.[Id], a.[Name]
   FROM Books b inner join Authors a on a.[Id] = b.[AuthorId]
-  WHERE @SearchString IS NULL 
+  WHERE @SearchString IS NULL OR LEN(@SearchString) < @MinLenghForSearch
   OR LOWER(b.[Title]) LIKE CONCAT('%', LOWER(@SearchString), '%')
   OR LOWER(a.[Name]) LIKE CONCAT('%', LOWER(@SearchString), '%')
+  OR b.TableOfContents.exist('//*[contains(text()[1], sql:variable(""@SearchString""))]') = 1
   ORDER by  b.[Id]
-  OFFSET @PageStartsZero * @PerPage  ROWS FETCH NEXT @PerPage ROWS ONLY
+  OFFSET @PageStartsZero * @PerPage  ROWS FETCH NEXT @PerPage ROWS ONLY;
 END
 ");
         }
