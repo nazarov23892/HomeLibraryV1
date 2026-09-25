11c30517c0ecf5e1510a7c7efd965c24950deee1
NazarovMikhail <nazarovmu@monqlab.com>
2026-09-25 08:24:33 +0500

    + Хранимая процедура удаления книги по Id.

---
+++ HomeLibrary.DAL/Migrations/20260925031948_m008_AddSpBookDelete.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeLibrary.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class m008_AddSpBookDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+@"
+DROP PROCEDURE IF EXISTS [dbo].[Books_Delete];
+
+GO
+
+CREATE PROCEDURE [dbo].[Books_Delete]
+    @Id               BIGINT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DELETE [Books]
+	WHERE [Id] = @Id
+    SELECT @@ROWCOUNT
+END;
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
